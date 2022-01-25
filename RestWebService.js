5768e8ea9f613c13eb7eb9d28d369196cc897210
Wookien <33688994+Wookien@users.noreply.github.com>
2022-01-25 18:52:31 +0100

    hmm

--- RestWebService.js
+++ RestWebService.js
@@ -39,9 +39,13 @@ app.get("/", (req, res) =>{
     res.send("hello world!");
  });
 
-app.get("/api/patient", (req, res) =>{
-    res.send(Patient);
+app.get("/api/patient/:id", (req, res) =>{
+    var  Patients = Patient.find(p => p.id === parseInt( req.params.id));
+    if(!Patients) res.status(404).send("This Patient with the given ID was not found");// 404 
+    res.send(Patients);
 });
+
+
 
 
 
