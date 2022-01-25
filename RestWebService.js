883f7a317befc04d94a6f4430d04dcc40c93732d
Wookien <33688994+Wookien@users.noreply.github.com>
2022-01-25 15:23:12 +0100

    nu køre det lidt

--- RestWebService.js
+++ RestWebService.js
@@ -1,7 +1,9 @@
+
+const http = require("http");
 const express = require('express');
 const app = express();
-const sqlite3 = require(sqlite3).verbose();
-const http = require("http");
+//const sqlite3 = require(sqlite3).verbose();
+
 app.use(express.json());
 
 
@@ -10,42 +12,36 @@ const apiwithout = "localhost:8080/getPatient?patientID=<CPR>&doctorID=<DOCTOR_W
 
 
 const admission = [
-    {medicalJournal:12345, department: Herning, Doctors: 3},
-    {medicalJournal: 2222, department: Herning, Doctors: 0},
+    {medicalJournal:12345, department: "Herning", Doctors: 3, id: 1},
+    {medicalJournal: 2222, department: "Herning", Doctors: 0, id: 2},
     
+];
+const admissionDoctorRelation = [
+    {Doctorid:12345, departmentid: 1},
+    {Doctorid:2222, departmentid: 2},
+
 ];
 
 const Patient = [
-    {id:12345, name: Sven},
-    {id:2222, name: MrMan},
+    {id:12345, name: "Sven"},
+    {id:2222, name:"MrMan"},
 
 ];
 
 const doctor = [
-    {id:1, name: Doctorwho, department: Herning},
-    {id:2, name: doctorlet, department: Herning},
-    {id:3, name: doctordoctor, department: Herning},
+    {id:1, name: "Doctorwho", department: "Herning"},
+    {id:2, name: "doctorlet", department: "Herning"},
+    {id:3, name: "doctordoctor", department: "Herning"},
 ];
-
-const server = http.createServer((req, res) => {
-    if (req.url === "/"){
-        res.write("hello");
-        res.end();
-    }
-    if(req.url === "/ api/courses"){
-        res.write(JSON.stringify([1,2,3]));
-        res.end();
-    }
-});
-server.listen(8080);
-console.log("Listenning on port 8080... ");
 
 
 app.get("/", (req, res) =>{
     res.send("hello world!");
-   });
+ });
 
-
-app.get("/api/courses", (req, res) =>{
-    res.send(courses);
+app.get("/api/patient", (req, res) =>{
+    res.send(Patient);
 });
+
+
+app.listen(8080, () => console.log("listenning on port 8080..."));
